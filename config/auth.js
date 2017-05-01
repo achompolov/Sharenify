5376150efd9065a03d742069b9566f6ecf72beda
achompolov <a.chompolov@gmail.com>
2017-05-01 17:43:08 +0300

    New project repository

---
+++ config/auth.js
@@ -0,0 +1,26 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '619846698210222',
+        'clientSecret'    : 'd8adaba88b92c99f6d365900729e0c23',
+        'callbackURL'     : 'http://sharenify.com/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.9/me?fields=first_name,last_name,email'
+
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'DW5daICN4k7SoS5IXvcqX5B8D',
+        'consumerSecret'     : '8wxuGkDBKBGjiRkVCoDZY2KgVQs5HvRWxwiwkTt7nbIxbKZGnI',
+        'callbackURL'        : 'http://sharenify.com/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : '960159422480-v57unhi6p5s4ommafep1b65skgq4bi64.apps.googleusercontent.com',
+        'clientSecret'     : 'EPv2YJr69i1uwJNAIf2TtMi4',
+        'callbackURL'      : 'http://sharenify.com/auth/google/callback'
+    }
+
+};
